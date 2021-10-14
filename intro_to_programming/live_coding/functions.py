9f80aceecc311a0f92887c04483c96464c734dc0
GabeHannafin <121328481@umail.ucc.ie>
2021-10-14 18:19:23 +0100

    live coding session

---
+++ intro_to_programming/live_coding/functions.py
@@ -0,0 +1,20 @@
+
+
+
+def func_1():
+    '''
+    docstring
+    '''
+
+    print("fuck you")
+
+    return
+
+# Default structure for a function
+
+while 1:
+
+    try:
+        func_1()
+    except Exception:
+        exit()
