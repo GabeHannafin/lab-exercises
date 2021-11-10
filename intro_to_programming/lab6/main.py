85f7b428c896c20e0e766083930afa628cadc1ec
GabeHannafin <121328481@umail.ucc.ie>
2021-11-10 17:41:27 +0000

    programming lab 6

---
+++ intro_to_programming/lab6/main.py
@@ -0,0 +1,26 @@
+# ScriptName: main.py
+# Author: Gabe
+
+# template for calling functions in another file
+
+# import my_functions from other files - different options
+# from my_functions import print_function
+# import my_functions - when you use this you need to call the function using 'my_functions.<function_name>'
+# this option imports all my_functions, using '*'
+from my_functions import *
+
+
+def main():
+    """
+    Call the functions defined in the functions.py file
+    """
+    print_function()
+    print(seasons(5))
+    # Something is broken here with question 2 on rotator
+    print(slice_reverse("abcda"))
+    print(add_to_list(5,5))
+
+
+if __name__ == "__main__":
+    ''' call the main() function in this file '''
+    main()
---
+++ intro_to_programming/lab6/my_functions.py
@@ -0,0 +1,74 @@
+# ScriptName: my_functions.py
+# Author: Gabe
+
+# template for calling functions in another file
+
+
+def print_function():
+    print("I'm in another file :)")
+
+def seasons(number):
+    """
+    I want you to rewrite the seasons() function from Lab 4. Within this
+    function, define a list of the seasons, and select the season based on the
+    input value where 1=Winter, 2=Spring, 3=Summer, and 4=Autumn. Remember list
+    indexing begins at 0. All other error message handling, param names, etc.,
+    in the original function, must be kept.
+    """
+    seasons = ['Winter', 'Spring', 'Summer', 'Autumn']
+    
+    if type(number) != int:    
+        return "Input value must be a number"
+
+    if number > 4 or number < 1:
+         return f"Number entered {number} is outside of input values"
+
+    else:
+        return seasons[number -1]
+
+def slice_reverse(input_value):
+    """
+    This question examines string slicing. Write a function,
+    slice_reverse(input_value), which determine if the input_value is a
+    palindrome i.e. reads the same backwards as forwards. The program should
+    return True or False (booleans) depending on the input. Do not use the
+    Python reverse() function
+    """ 
+    input_value = str(input_value)
+    print(input_value)
+    print(input_value[::-1])
+
+    if (input_value == input_value[::-1]):
+        return True
+
+    else:
+        return False
+
+def add_to_list(value="", list=[]):
+    """
+    I want you to create a function called add_to_list(value, list), which will
+    return a sorted list. This function will add value to the list if the list
+    does not already contain the value. For now, you can assume the list param
+    is already sorted. You can use the python function “sort()” to sort your
+    returning list. Sort() will not allow you to mix ints, floats and strings.
+    In your function set an appropriate default value for the list param.
+    Examples:
+    """
+    # add the value to the list if it is the correct type
+    try:
+        list.append(value)
+
+    except Exception:
+        return "Incorrect value defined for param list"
+    # attempt to sort the list, or return an error 
+    try:
+        list.sort()
+
+    except TypeError:
+        return "sort() does not like this mixture of elements"
+
+    return list
+   
+def add_to_list_no_sort(value, list):
+
+    
